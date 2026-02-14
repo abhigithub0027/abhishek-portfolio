5a0281f4a3cf5da91f532ab0e8dad5cbfaa6552e
Abhishek Yadav <abhiyadav5242@gmail.com>
2026-02-14 21:52:04 +0530

    Fix Vercel Flutter build

--- vercel-build.sh
+++ vercel-build.sh
@@ -1,9 +1,18 @@
 #!/bin/bash
+set -e
 
-# Install Flutter
-git clone https://github.com/flutter/flutter.git --depth 1
+echo "Installing Flutter..."
+
+git clone https://github.com/flutter/flutter.git --depth 1 -b stable
 export PATH="$PATH:`pwd`/flutter/bin"
 
+flutter --version
+
+echo "Enable web..."
 flutter config --enable-web
+
+echo "Get packages..."
 flutter pub get
+
+echo "Build web..."
 flutter build web --release
